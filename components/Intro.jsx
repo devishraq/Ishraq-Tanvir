ba342d98ba3df33951f56b5f11712389eb6bafbe
Ishraq Tanvir <101325013+devishraq@users.noreply.github.com>
2023-10-19 19:40:47 +0000

    Icon link connected

--- components/Intro.jsx
+++ components/Intro.jsx
@@ -1,11 +1,11 @@
 export default () => {
-  return (
-    <h2 className="text-3xl xs:text-4xl mb-8 font-thin flex">
-      <div className="z-50 min-h-max w-[2px] rounded-md bg-yellow-300 mr-6 px-[2.5px]"></div>
-      I am &nbsp;
-      <span className="font-normal underline underline-offset-8 text-yellow-300 ">
-        Ishraq Tanvir.
-      </span>
-    </h2>
-  );
+ return (
+  <h2 className='text-3xl xs:text-4xl mb-8 font-thin flex  z-[11100]'>
+   <div className='z-50 min-h-max w-[2px] rounded-md bg-yellow-300 mr-6 px-[2.5px]'></div>
+   I am &nbsp;
+   <span className='font-normal underline underline-offset-8 text-yellow-300 '>
+    Ishraq Tanvir.
+   </span>
+  </h2>
+ );
 };
