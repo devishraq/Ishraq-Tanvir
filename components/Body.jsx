03027f582f8f3fb44da09313799c5f73be2ea85f
Ishraq Tanvir <101325013+devishraq@users.noreply.github.com>
2024-07-11 13:50:16 +0600

    Update Body.jsx
    Some content updates in main body!

--- components/Body.jsx
+++ components/Body.jsx
@@ -1,39 +1,33 @@
 export default () => {
   return (
-    <div className="md:w-[600px] text-slate-100 ">
+    <div className="md:w-[600px] text-slate-100">
       <div className="text-lg xs:text-[21px] leading-7 xs:leading-[34px] flex">
         <div className="z-50 w-[2px] rounded-md bg-rose-500 mr-6 px-[2.5px]"></div>
         <div>
-          I'm an independent developer who just kickstarted my career in web and
-          beyond. I'm passionate about exploring web technologies and more
-          deeper of web. My tech stack includes{" "}
+          I'm an independent developer passionate about web technologies and beyond. 
+          I love exploring the deeper aspects of the web, constantly pushing the boundaries of what's possible.
+          My tech stack includes{" "}
           <span className="bg-sky-400 span-body">React</span>,{" "}
           <span className="bg-green-600 span-body">Node.js</span>,{" "}
-          <span className="bg-slate-50 span-body tex">Deno</span>,{" "}
+          <span className="bg-slate-50 span-body">Deno</span>,{" "}
           <span className="bg-[#c2c2c2] span-body">Express.js</span>,{" "}
           <span className="bg-[#b4ca65] span-body">EJS</span>,{" "}
           <span className="bg-sky-500 span-body">tailwindcss</span>,{" "}
-          <span className="bg-[#007FFF] span-body">Material UI</span>,{" "}
-          <span className="bg-[#6610f2] span-body">Bootstrap</span>,{" "}
           <span className="bg-[#00ed64] span-body">MongoDB</span>,{" "}
           <span className="bg-red-500 span-body">Redis</span>,{" "}
           <span className="bg-cyan-500 span-body">PostgreSQL</span>,{" "}
           <span className="bg-slate-50 span-body">Three.js</span>,{" "}
           <span className="bg-[#E10098] span-body">GraphQL</span>,{" "}
-          <span className="bg-[#9587c0] span-body">Graph CMS</span> ,
-          <span className="bg-[#c69] span-body">Sass</span> ,{" "}
-          <span className="bg-white span-body">Next.js</span>. Motivated by
-          project building, I am trying to create and ship{" "}
+          <span className="bg-white span-body">Next.js</span>
+          . My main goal is to{" "}
           <span className="underline underline-offset-4 decoration-rose-400">
-            new projects on GitHub every week
-          </span>
-          . My main target is to{" "}
-          <span className="underline underline-offset-4 decoration-rose-400">
-            make new world design and user interaction for the web with the
-            superb power of AI & ML
-          </span>
-          . Beyond programming, I have a keen interest in physics and math,
-          seeking to solve challenging problems.
+            create a new era of web design and user interaction powered by AI & ML.
+          </span>{" "}
+          I aim to develop innovative tools and frameworks that enhance developer productivity and user experience.
+          <br />
+          Beyond programming, I have a keen interest in physics and math, seeking to solve challenging problems. 
+          I am also preparing for the International Olympiad in Informatics (IOI) and the International Mathematical Olympiad (IMO), 
+          constantly challenging myself to excel.
         </div>
       </div>
     </div>
